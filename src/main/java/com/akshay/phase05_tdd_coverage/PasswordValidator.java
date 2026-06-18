6f6eaa49cb70958fbb8648ed3ad65d9c5750b9d3
Akshay Patil <akshaypatil00404@gmail.com>
2026-06-18 12:56:10 +0530

    feat: GREEN - return true to pass valid password test

---
+++ src/main/java/com/akshay/phase05_tdd_coverage/PasswordValidator.java
@@ -0,0 +1,12 @@
+// PasswordValidator.java
+package com.akshay.phase05_tdd_coverage;
+
+public class PasswordValidator {
+	
+	private PasswordValidator() {}
+	
+	public static boolean isValid(String password) {
+		return true;
+	}
+
+}
